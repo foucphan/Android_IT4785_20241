859a534ccf00ef2cc7e0509703741311ce22d3eb
P D Can <Pdc@gmail.com>
2024-10-10 22:43:51 +0700

    add BTVN 3_10

---
+++ BTVN 3_10/LinearLayout/Calculator_LinearLayout/app/src/main/java/com/example/calculator_linearlayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calculator_linearlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Liên kết giao diện activity_main.xml
+    }
+}
